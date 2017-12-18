50d38a9d38b9255e7c616271b7a28f912a189a61
oshinyil <taneyoroshi@gmail.com>
2017-12-18 15:30:46 +0800

    Imported dataset.

--- 1-data-preprocessing/data_preprocessing_template.py
+++ 1-data-preprocessing/data_preprocessing_template.py
@@ -4,6 +4,13 @@ Created on Mon Dec 18 15:01:55 2017
 
 @author: Tane Yoroshi
 """
+
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
